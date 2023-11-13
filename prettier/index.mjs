5f909abd4c71822abb391c1089f236906b3d0485
Timo Bechtel <hi@timobechtel.com>
2023-11-13 13:49:14 +0100

    docs(prettier): add comment about tabs vs spaces

--- prettier/index.mjs
+++ prettier/index.mjs
@@ -8,6 +8,9 @@ const overridableDefaults = {
   endOfLine: 'lf',
   tabWidth: 2,
   printWidth: 80,
+  // Why I'd generally prefer tabs, as they are a lot more customizable,
+  // I've found that in a lot of places, tabs are displayed as 8 spaces by default,
+  // which makes it hard to read on mobile devices. (e.g. the GitHub app)
   useTabs: false,
 };
 
